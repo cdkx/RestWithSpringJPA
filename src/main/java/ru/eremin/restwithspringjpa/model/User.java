b9d66bcce147a4528f2d5c59e9083e09a9c4e7af
Viktor Eremin <vektor451@gmail.com>
2025-06-09 05:39:26 +0300

    Column email nullable = false

--- src/main/java/ru/eremin/restwithspringjpa/model/User.java
+++ src/main/java/ru/eremin/restwithspringjpa/model/User.java
@@ -33,7 +33,7 @@ public class User {
     @Column(name = "name")
     private String name;
 
-    @Column(name = "email",  unique = true)
+    @Column(name = "email",  unique = true, nullable = false)
     private String email;
 
     @Column(name = "age")
